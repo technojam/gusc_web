4df44ec8c8f93077ea1669d6bb61ed12b3d8a7ed
DevSankalp <94849180+DevSankalp@users.noreply.github.com>
2022-12-29 14:14:50 +0530

    Latest Events Component - Added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import ClubComponent from './Components/ClubComponent';
 import ClubInfo from './Pages/ClubInfo';
+import LatestEventComponent from './Components/LatestEvents'
 
 function App() {
   return (
     <div className='h-32'>
       {/* <ClubComponent /> */}
-
       <ClubInfo />
+      <LatestEventComponent />
     </div>
   );
 }
