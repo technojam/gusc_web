7f3d4019db4d45197e479b346efb4ea0cb27e531
M Sami <beboolean@gmail.com>
2023-11-03 18:47:46 +0530

    added components and some fixed

---
+++ src/Pages/EventPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EventPage = () => {
+  return (
+    <div>
+      <div className="bg-red-700 p-12 text-center text-white">
+        <p className="text-4xl">Galgotias University Student Council</p>
+        <p className="text-7xl font-bold mt-2">Events</p>
+      </div>
+    </div>
+  )
+}
+
+export default EventPage
---
+++ src/Pages/Gallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div>
+        <div className="bg-red-700 p-12 text-center text-white">
+        <p className="text-4xl">Galgotias University Student Council</p>
+        <p className="text-7xl font-bold mt-2">Gallery</p>
+      </div>
+    </div>
+  )
+}
+
+export default Gallery
