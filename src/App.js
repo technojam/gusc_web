43b1a4af780ff1b53ee6884871d1089f3f7d2f7c
Mohammad Sami Bhat <77672838+MSami625@users.noreply.github.com>
2023-11-03 15:59:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import React from "react";
-import "./App.css";
-// import ClubComponent from "./Components/ClubComponent";
-// import ClubInfo from "./Pages/ClubInfo";
-// import LatestEventComponent from "./Components/LatestEvents";
-// import Core from "./Components/CoreTeam";
-import Footer from "./Components/Footer";
-// import Hero from "./Pages/Hero";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
-// import Achievements from "./Components/Achievements";
-import Clubs from "./Components/Clubs";
-import Main from "./Components/Main";
-import EventPage from "./Components/EventPage";
-import TeamPage from "./Components/TeamPage"
-import ClubTeamPage from "./Components/ClubTeamPage";
-import CoreSCTeam from "./Components/CoreSCTeam";
+import React from 'react';
+import './App.css';
+import ClubInfo from './Pages/ClubInfo';
+import LatestEventComponent from './Components/LatestEvents';
+import Core from './Components/CoreTeam';
+import Footer from './Components/Footer';
+import Hero from './Pages/Hero';
+import Navbar from './Components/Navbar';
+
+import clubsData from "./data/clubData.json"
+
+import {
+  Routes,
+  Route,
+  BrowserRouter
+} from "react-router-dom";
+import Achievements from './Components/Achievements';
+import TeamPage from './Pages/TeamPage';
+
+import member from "./data/core-team.json"
+
 function App() {
   return (
-    <div className="h-32">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="clubs" element={<Clubs />} />
-        <Route path="events" element={<EventPage />} />
-        <Route path ="corePage" element={<TeamPage/>}/>
-        <Route path = "clubPage" element ={<ClubTeamPage/>}/>
-        <Route path ="coreSCPage" element ={<CoreSCTeam/>}/>
-      </Routes>
-      <Footer />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={  
+            <>
+              <Navbar />
+              <Hero />
+              <Achievements />
+              <LatestEventComponent />
+              <Core />
+              <ClubInfo clubs={clubsData.clubs}/>
+              <Footer />
+            </>
+          } />
+          <Route path='/teamPage' element={<TeamPage member={member.members}/>} /> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
